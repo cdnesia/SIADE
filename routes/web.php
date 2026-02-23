4bb619cfb6a35210fc084a5b15ec67e6a50cb5a0
mjumain93 <mjumain93@gmail.com>
2026-02-23 16:50:09 +0700

    repair beasiwa

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\SyncController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/login', 301);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
