f0c99fb91783ff96154e4f2dcda436481fcec5d3
mjumain93 <mjumain93@gmail.com>
2026-02-25 05:25:37 +0700

    repair api

--- app/Http/Controllers/Api/MasterApiController.php
+++ app/Http/Controllers/Api/MasterApiController.php
@@ -8,22 +8,5 @@ use Illuminate\Support\Facades\DB;
 
 class MasterApiController extends Controller
 {
-    public function dataDosen()
-    {
-        $data = DB::connection('siade_old')->table('pegawai')->where('NA', 'A')->get();
-        return response()->json([
-            'success' => true,
-            'data' => $data
 
-        ]);
-    }
-    public function dataRuang()
-    {
-        $data = DB::connection('siade_old')->table('ruang')->where('NA', 'A')->get();
-        return response()->json([
-            'success' => true,
-            'data' => $data
-
-        ]);
-    }
 }
